99cd3ae6850cd1556ca9fad4e5d4a5612d5b3790
victor <vcarulla@technisys.com>
2018-05-07 03:29:36 -0300

    Refactor de la aplicación.

---
+++ controllers/product.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const productModel = require('../models/product');
+
+function getProducts(req, res) {
+    console.log(`GET '/api/product'`);
+
+    productModel.find({}, (err, products) => {
+        if (err) return res.status(500).send({ message: `Se produjo un error al realizar la petición. ERROR: ${err}` });
+        if (!products) return res.status(404).send({ message: `No se encontraron coincidencias. ERROR: ${err}` });
+
+        res.status(200).send({ products });
+    });
+}
+
+function getProduct(req, res) {
+    console.log(`GET '/api/product/${req.params.productId}'`);
+
+    let productId = req.params.productId;
+
+    productModel.findById(productId, (err, product) => {
+        if (err) return res.status(500).send({ message: `Se produjo un error al realizar la petición. ERROR: ${err}` });
+        if (!product) return res.status(404).send({ message: `No se encontraron coincidencias. ERROR: ${err}` });
+
+        res.status(200).send({ product });
+
+    });
+}
+
+function updateProduct(req, res) {
+    console.log(`PUT '/api/product/${req.params.productId}'`);
+
+    let productId =  req.params.productId;
+    let body = req.body;
+
+    productModel.findByIdAndUpdate(productId, body, (err, productUpdated) => {
+        if (err) return res.status(500).send({ message: `Se produjo un error al realizar la petición. ERROR: ${err}` });
+        if (!productUpdated) return res.status(404).send({ message: `No se encontraron coincidencias. ERROR: ${err}` });
+
+        res.status(200).send({ message: `El id: ${productId} fue actualizado con éxito. ${productUpdated}`});
+    });
+}
+
+function deleteProduct(req, res) {
+    console.log(`DELETE '/api/product/${req.params.productId}'`);
+
+    let productId =  req.params.productId;
+
+    productModel.findById(productId, (err, productDeleted) => {
+        if (err) return res.status(500).send({ message: `Se produjo un error al realizar la petición. ERROR: ${err}` });
+        if (!productDeleted) return res.status(404).send({ message: `No se encontraron coincidencias.. ERROR: ${err}` });
+
+        productDeleted.remove( err => {
+            if (err) return res.status(500).send({ message: `Se produjo un error intentando borrar en la db. ERROR: ${err}` });
+            res.status(200).send({ message: `El id: ${productId} fue eliminado con éxito`});
+        });
+    });
+}
+
+function saveProduct(req, res) {
+    console.log(`POST '/api/product': \n`, req.body);
+
+    let product = new productModel();
+    product.name = req.body.name;
+    product.name = req.body.name;
+    product.price = req.body.price;
+    product.picture = req.body.picture;
+    product.description = req.body.description;
+    product.category = req.body.category;
+
+    product.save( (err, productStored) => {
+        if (err) res.status(500).send({ message: `Se produjo un error intentando guardar en la db. ERROR: ${err}` });
+        res.status(200).send({ product: productStored });
+    });
+}
+
+module.exports = {
+    getProduct,
+    getProducts,
+    updateProduct,
+    deleteProduct,
+    saveProduct
+};
--- index.js
+++ index.js
@@ -1,94 +1,17 @@
-'use strict'
+'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const app = require('./app');
+const config = require('./config');
 
-const Product = require('./models/product');
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded( {extended: false} ));
-app.use(bodyParser.json());
-
-app.get('/api/product', (req, res) => {
-    Product.find({}, (err, products) => {
-        if (err) return res.status(500).send({ message: `Se produjo un error al realizar la petición. ERROR: ${err}` });
-        if (!products) return res.status(404).send({ message: `No se encontraron coincidencias. ERROR: ${err}` });
-
-        res.status(200).send({ products});
-    });
-});
-
-app.get('/api/product/:productId', (req, res) => {
-    let productId = req.params.productId;
-
-    Product.findById(productId, (err, product) => {
-        if (err) return res.status(500).send({ message: `Se produjo un error al realizar la petición. ERROR: ${err}` });
-        if (!product) return res.status(404).send({ message: `No se encontraron coincidencias. ERROR: ${err}` });
-
-        res.status(200).send({ product});
-
-    });
-});
-
-app.post('/api/product', (req, res) => {
-    console.log(`POST /api/product`);
-    console.log(req.body);
-
-    let product = new Product();
-    product.name = req.body.name;
-    product.name = req.body.name;
-    product.price = req.body.price;
-    product.picture = req.body.picture;
-    product.description = req.body.description;
-    product.category = req.body.category;
-
-    product.save( (err, productStored) => {
-        if (err) res.status(500).send({ message: `Se produjo un error intentando guardar en la db. ERROR: ${err}` });
-        res.status(200).send({ product: productStored });
-    });
-});
-
-app.put('/api/product/:productId', (req, res) => {
-    let productId =  req.params.productId;
-    let body = req.body;
-
-    Product.findByIdAndUpdate(productId, body, (err, productUpdated) => {
-        if (err) return res.status(500).send({ message: `Se produjo un error al realizar la petición. ERROR: ${err}` });
-        if (!productUpdated) return res.status(404).send({ message: `No se encontraron coincidencias.. ERROR: ${err}` });
-
-        res.status(200).send({ message: `El id: ${productId} fue acctualizado con exito. ${productUpdated}`});
-
-    });
-});
-
-app.delete('/api/product/:productId', (req, res) => {
-    let productId =  req.params.productId;
-
-    Product.findById(productId, (err, productDeleted) => {
-        if (err) return res.status(500).send({ message: `Se produjo un error al realizar la petición. ERROR: ${err}` });
-        if (!productDeleted) return res.status(404).send({ message: `No se encontraron coincidencias.. ERROR: ${err}` });
-
-        product.remove( (err) => {
-            if (err) return res.status(500).send({ message: `Se produjo un error intentando borrar en la db. ERROR: ${err}` });
-            res.status(200).send({ message: `El id: ${productId} fue eliminado con exito`});
-        });
-
-
-    });
-
-});
-
-mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+mongoose.connect(config.db, (err, res) => {
     if (err) {
-     return console.error(`Error en la conexión con mongodb.`, `ERROR: ${err}`) ;
+        return console.error(`Error en la conexión con mongodb.\nERROR: ${err}`);
     }
     console.info(`Conexión a mongodb establecida correctamente.`);
 
-    app.listen(port, () => {
-        console.info(`API REST corriendo en http://localhost:${port}.`);
+    app.listen(config.port, () => {
+        console.info(`API REST corriendo en http://localhost:${config.port}`);
     });
 });
 
