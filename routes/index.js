83a7c2b2e37cb9115944508f50917cfb829ff0c0
victor <vcarulla@technisys.com>
2018-05-20 00:32:51 -0300

    quito middleware de getProduct

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const auth =  require('../middlewares/auth');
 const api = express.Router();
 
 api.get( '/product', productCtrl.getProducts );
-api.get( '/product/:productId', auth, productCtrl.getProduct );
+api.get( '/product/:productId', productCtrl.getProduct );
 api.post( '/product', auth, productCtrl.saveProduct );
 api.put( '/product/:productId', auth, productCtrl.updateProduct );
 api.delete( '/product/:productId', auth, productCtrl.deleteProduct );
